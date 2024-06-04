f20b57faf45535bcdac8b845cf342504f2d80ebe
RichardHerz <rich@reactorlab.net>
2024-06-03 22:42:23 -0700

    work on button added only on second click
    in future, need to change cursor shape between clicks and add new button at mouse loc

--- ex_add_elements/js/process_main.js
+++ ex_add_elements/js/process_main.js
@@ -5,8 +5,32 @@
   https://www.gnu.org/licenses/gpl-3.0.en.html
 */
 
+var optClicked = 0;
+
+function fixedOneClicked(event) {
+  console.log('button_fixedOne clicked, event = ' + event);
+  let modkey = event.getModifierState("Alt"); // Alt is Option on Mac
+  console.log('fixedOneClicked, mod key = ' + modkey);
+  if (modkey) {
+    optClicked = 1;
+    console.log('optClicked = ' + optClicked);
+  }
+}
+
 function innerDivClicked(event) {
-  console.log('inner div was clicked, event = ' + event);
+  // innerDiv got click when button fixedOne clicked, so wait for next click on innerDiv to add button 
+  console.log('inner div was clicked, optClicked = ' + optClicked);
+  if (optClicked == 1) {
+    console.log('optClicked is 1? = ' + optClicked);
+    optClicked = 2;
+    console.log('optClicked set to 2');
+  } else if (optClicked == 2) {
+    console.log('optClicked is 2? = ' + optClicked);
+    optClicked = 0;
+    console.log('optClicked set to 0 & add button');
+    document.getElementById("div_outerdiv").
+    innerHTML += '<button type="button" id="button_newOne" onclick="newOneClicked(event)">newOne</button>';
+  }
 }
 
 function outerDivClicked(event) {
@@ -21,30 +45,23 @@ function newerOneClicked(event) {
   console.log('button newerOne clicked, event = ' + event);
 }
 
-function fixedOneClicked(event) {
+  // // A COUPLE WAYS TO ADD NEW ELEMENT
 
-  console.log('button_fixedOne clicked, event = ' + event);
+  // if (modkey) {
+  //   document.getElementById("div_outerdiv").
+  //     innerHTML += '<button type="button" id="button_newOne" onclick="newOneClicked(event)">newOne</button>';
+  // }
 
-  let modkey = event.getModifierState("Alt"); // Alt is Option on Mac
-  console.log('mod key = ' + modkey);
+  // if (modkey) {
+  //   const newD = document.createElement("div");
+  //           newD.innerText = "Hello World";
+  //           document.body.appendChild(newD);
+  // }
 
-  // A COUPLE WAYS TO ADD NEW ELEMENT
+  // if (modkey) {
+  //   const newD = document.createElement("div");
+  //           newD.innerHTML = '<button type="button" id="button_newerOne" onclick="newerOneClicked(event)">newerOne</button>';
+  //           document.body.appendChild(newD);
+  // }
 
-  if (modkey) {
-    document.getElementById("div_outerdiv").
-      innerHTML += '<button type="button" id="button_newOne" onclick="newOneClicked(event)"</button>';
-  }
 
-  if (modkey) {
-    const newD = document.createElement("div");
-            newD.innerText = "Hello World";
-            document.body.appendChild(newD);
-  }
-
-  if (modkey) {
-    const newD = document.createElement("div");
-            newD.innerHTML = '<button type="button" id="button_newerOne" onclick="newerOneClicked(event)">newerOne</button>';
-            document.body.appendChild(newD);
-  }
-
-}
