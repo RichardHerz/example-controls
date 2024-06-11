4895088ed2ff5ec54dd8612321a0bc2f4fc53099
RichardHerz <rich@reactorlab.net>
2024-06-11 11:53:36 -0700

    remove console.log()

--- ex_add_elements/js/process_main.js
+++ ex_add_elements/js/process_main.js
@@ -11,13 +11,9 @@ var clickedClass = '';
 var clickedID = '';
 
 function fixedClicked(event) {
-  console.log('fixedClicked(), event = ' + event);
-  console.log('target id = ' + event.target.id)
-  console.log('target className = ' + event.target.className)
   clickedClass = event.target.className;
   clickedID = event.target.id;
   let modkey = event.getModifierState("Alt"); // Alt is Option on Mac
-  console.log('fixedClicked, mod key = ' + modkey);
   if (modkey) {
     optClicked = 1; // toggles to 0 in canvasDivClicked()
     let el = document.getElementById(clickedID);
@@ -30,24 +26,17 @@ function fixedClicked(event) {
 // NOTE: "canvas" is used here for convenience and does NOT mean an HTML canvas element
 
 function canvasDivClicked(event) { 
-  console.log('canvasDiv was clicked, optClicked = ' + optClicked);
   if (optClicked == 1) {
-    console.log('optClicked is 1? = ' + optClicked);
     optClicked = 0; // toggles to 1 in fixedClicked() 
   
     elemCounter += 1; 
-    console.log('elemCounter ON ADD = ' + elemCounter);
     let el = document.getElementById("div_canvasDiv");
 
     let newID = clickedID + elemCounter;
-    console.log('newID = ' + newID);
-
     let newClass = clickedClass;
-    console.log('newClass = ' + newClass);
 
     let x = event.clientX + 'px';
     let y = event.clientY + 'px';
-    console.log('x,y = ' + x + ', ' + y);
 
     el.innerHTML += '<button type="button" class=" '+clickedClass+' " id=" '+newID
       +' " style="top: '+y+'; left: '+x+';" onclick="canvasClicked(event)">'
@@ -61,12 +50,8 @@ function canvasDivClicked(event) {
 }
 
 function canvasClicked(event) {
-  console.log('canvasClicked(), event = ' + event);
   let clickedID = event.target.id;
-  console.log('event.target.id = ' + event.target.id);
-  console.log('event.target.className = ' + event.target.className);
   let modkey = event.getModifierState("Alt"); // Alt is Option on Mac
-  console.log('mod key = ' + modkey);
   if (modkey) {
     let el = document.getElementById(clickedID);
     el.remove();
